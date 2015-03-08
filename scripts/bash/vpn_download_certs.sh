a03b9b8292d2e5ef5786baf5efe76eda5b95346e
rhiw <daltonnikitas@gmail.com>
2015-03-08 00:16:09 -0500

    Made ec2 private key a parameter to vpn_download_certs.sh

--- scripts/bash/vpn_download_certs.sh
+++ scripts/bash/vpn_download_certs.sh
@@ -2,6 +2,7 @@
 
 S3_OBJECT=$1
 DESTINATION_DIR=$2
+EC2_PRIVATE_KEY=$3
 
 #Create destination dir(s)
 mkdir -p $DESTINATION_DIR
@@ -11,7 +12,7 @@ aws s3 cp s3://$S3_OBJECT .
 tar -xvf client_materials.tgz
 
 #Decrypt and place contents
-openssl rsautl -decrypt -inkey ~/Keys/VPC.pem -in client_materials/vpn_secret.encrypted -out client_materials/vpn_secret
+openssl rsautl -decrypt -inkey $EC2_PRIVATE_KEY -in client_materials/vpn_secret.encrypted -out client_materials/vpn_secret
 openssl enc -d -out $DESTINATION_DIR/ca.crt -in client_materials/ca.crt.encrypted -aes256 -k vpn_secret
 openssl enc -d -out $DESTINATION_DIR/client0.crt -in client_materials/client0.crt.encrypted -aes256 -k vpn_secret
 openssl enc -d -out $DESTINATION_DIR/client0.key -in client_materials/client0.key.encrypted -aes256 -k vpn_secret
