d05bcfef8cc303544729c21b989730f5ebd3164d
rhiw <daltonnikitas@gmail.com>
2015-03-08 00:48:12 -0500

    Cleaned up vpn.sh, added absolute paths

--- scripts/bash/vpn.sh
+++ scripts/bash/vpn.sh
@@ -7,7 +7,7 @@ VPN_SERVER_CONFIG_S3_PATH=$2
 DESTINATION_S3_PREFIX=$3
 
 #I don't know if this NAT stuff is necessary on VPN server, but it seems like it might be and definitely works
-eval ./nat.sh
+/home/ec2-user/yggdrasil-master/scripts/bash/nat.sh
 
 #Get EasyRSA 
 mkdir /etc/openvpn/easy-rsa
@@ -19,9 +19,9 @@ cp -r EasyRSA-2.2.2/* /etc/openvpn/easy-rsa/
 #Set up certs and keys
 if [ $VPN_SERVER_CERTS_S3_PREFIX = None ]
     then
-        eval ./vpn_generate_certs.sh $DESTINATION_S3_PREFIX
+        /home/ec2-user/yggdrasil-master/scripts/bash/vpn_generate_certs.sh $DESTINATION_S3_PREFIX
     else
-        eval "aws s3 cp s3://"$VPN_SERVER_CERTS_S3_PREFIX" /etc/openvpn/easy-rsa/keys --recursive"
+        aws s3 cp s3://$VPN_SERVER_CERTS_S3_PREFIX /etc/openvpn/easy-rsa/keys --recursive
 fi
 
 #Set up config
@@ -29,10 +29,9 @@ if [ $VPN_SERVER_CONFIG_S3_PATH = None ]
     then
         cp /home/ec2-user/yggdrasil-master/templates/server.conf /etc/openvpn/server.conf
     else
-        eval "aws s3 cp s3://"$VPN_SERVER_CONFIG_S3_PATH" /etc/openvpn"
+        aws s3 cp s3://$VPN_SERVER_CONFIG_S3_PATH /etc/openvpn
 fi
 
 service openvpn start
 exit 0
-
 
