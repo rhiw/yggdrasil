032fcaa622b994053da68d813f9aa7958065b84f
rhiw <daltonnikitas@gmail.com>
2015-03-08 00:07:51 -0500

    Added proper passing of destination s3 prefix

--- scripts/bash/vpn.sh
+++ scripts/bash/vpn.sh
@@ -4,6 +4,7 @@ set -x
 
 VPN_SERVER_CERTS_S3_PREFIX=$1
 VPN_SERVER_CONFIG_S3_PATH=$2
+DESTINATION_S3_PREFIX=$3
 
 #I don't know if this NAT stuff is necessary on VPN server, but it seems like it might be and definitely works
 eval ./nat.sh
@@ -18,7 +19,7 @@ cp -r EasyRSA-2.2.2/* /etc/openvpn/easy-rsa/
 #Set up certs and keys
 if [ $VPN_SERVER_CERTS_S3_PREFIX = None ]
     then
-        eval ./vpn_generate_certs.sh
+        eval ./vpn_generate_certs.sh $DESTINATION_S3_PREFIX
     else
         eval "aws s3 cp s3://"$VPN_SERVER_CERTS_S3_PREFIX" /etc/openvpn/easy-rsa/keys --recursive"
 fi
