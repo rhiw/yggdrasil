949e778f1c9a418fdc8751e788bbba15038e2644
rhiw <daltonnikitas@gmail.com>
2015-03-15 00:49:53 -0400

    Added LVM stuff to download server

--- scripts/bash/download.sh
+++ scripts/bash/download.sh
@@ -31,3 +31,22 @@ pip install ipython
 pip install boto
 pip install requests
 
+#Set up LVM
+echo "Start doing lvm stuff"
+apt-get install lvm2 -y
+vgcreate download_group /dev/xvdh
+lvcreate --name logical_download -l 100%VG download_group
+mkfs -t ext4 /dev/download_group/download_media
+mount /dev/download_group/download_media /download
+echo "Stop doing lvm stuff"
+
+#set volume resizer cron
+COMMAND='source /home/ubuntu/.bashrc && workon py34 && python /home/ubuntu/yggdrasil-master/scripts/python/volume_resizer.py /media .8'
+JOB="*/5 * * * * root $COMMAND 2>&1 | /usr/bin/logger -t media_volume_resizer"
+
+cat << EOF > /etc/cron.d/volume_resizer
+$JOB
+
+EOF
+
+exit 0
